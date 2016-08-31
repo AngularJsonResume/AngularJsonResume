28818f9bf8877dbb34be4da702de26746674e936
alexwander <ryndyuk.alexander@gmail.com>
2016-08-31 20:55:08 +0300

    app.module init commits

---
+++ app/app.module.ts
@@ -0,0 +1,11 @@
+import {AppComponent} from './app.component';
+
+@NgModule({
+    imports:      [ BrowserModule ],
+    declarations: [ AppComponent,
+
+    ],
+   
+    bootstrap:    [ AppComponent ]
+})
+export class AppModule { }
