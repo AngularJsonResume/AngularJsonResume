8165458553b2600bce80467f1de99bd74920b5c3
alexwander <ryndyuk.alexander@gmail.com>
2016-09-03 20:56:06 +0300

    bootstrapping app

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,11 +1,27 @@
+import { NgModule, CUSTOM_ELEMENTS_SCHEMA }      from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { HttpModule } from '@angular/http';
+import {FormsModule} from '@angular/forms';
+
 import {AppComponent} from './app.component';
 
-@NgModule({
-    imports:      [ BrowserModule ],
-    declarations: [ AppComponent,
 
+import {DemoService} from './services/demo.service'
+import {NavbarComponent} from './components/navbar/navbar.component';
+import {JumbotronComponent} from './components/jumbotron/jumbotron.component';
+import {HomeComponent} from './components/pages/home.component';
+import {AboutComponent} from './components/pages/about.component';
+
+@NgModule({
+    imports:      [ BrowserModule, HttpModule, FormsModule ],
+    declarations: [ AppComponent,
+        NavbarComponent,
+        JumbotronComponent,
+        HomeComponent,
+        AboutComponent
     ],
-   
+    providers: [DemoService],
+    schemas: [CUSTOM_ELEMENTS_SCHEMA],
     bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
