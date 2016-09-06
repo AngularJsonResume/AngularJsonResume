8bb2e1a733bb0c859ce8c1d5056c6355897883dd
alexwander <ryndyuk.alexander@gmail.com>
2016-09-06 20:59:26 +0300

    component add template

--- app/app.component.ts
+++ app/app.component.ts
@@ -3,7 +3,9 @@ import { Component } from '@angular/core';
 
 
 @Component({
-
+    moduleId: module.id,
+    selector: 'my-app',
+    templateUrl: 'app.component.html'
 })
 export class AppComponent {
 
