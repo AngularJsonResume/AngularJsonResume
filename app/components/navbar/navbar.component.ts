00337cb978b1ab9f82e2bfe147c71365477539ba
alexwander <ryndyuk.alexander@gmail.com>
2017-02-23 21:01:41 +0200

    navbar component impl

---
+++ app/components/navbar/navbar.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+import {DemoService} from '../../services/demo.service';
+import {Observable} from 'rxjs/Rx';
+
+@Component({
+    moduleId:module.id,
+    selector: 'navbar',
+    templateUrl: 'navbar.component.html'
+})
+export class NavbarComponent {
+
+
+
+
+
+}
