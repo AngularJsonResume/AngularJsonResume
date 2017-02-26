92fac41654a5a45ad8763cf81d47c51f544b9e90
alexwander <ryndyuk.alexander@gmail.com>
2017-02-26 14:29:51 +0200

    home component init

--- app/components/pages/home.component.ts
+++ app/components/pages/home.component.ts
@@ -1,1 +1,21 @@
+import { Component } from '@angular/core';
+import {DemoService} from '../../services/demo.service';
+import {Observable} from 'rxjs/Rx';
+
+@Component({
+    moduleId:module.id,
+    selector: 'home',
+    templateUrl: 'home.component.html'
+})
+export class HomeComponent {
+
+
+    public peoples;
+
+
+
+  
+
+
+}
 
